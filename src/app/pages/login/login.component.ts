636b067cbf1bf37e6c079dc39d07e3f668a32901
Rafael Honorato <contatorafaelhonorato@gmail.com>
2026-08-20 15:03:07 -0300

    feat: Tela de login básica

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,11 +1,84 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, inject, signal } from '@angular/core';
+import {
+  FormBuilder,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+import { MatButton } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { UserService } from '@app/core/services/user.service';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [
+    ReactiveFormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatIconModule,
+    MatButton,
+    MatProgressSpinnerModule,
+  ],
   templateUrl: './login.component.html',
-  styleUrl: './login.component.css'
+  styleUrl: './login.component.css',
 })
 export class LoginComponent {
+  private readonly fb = inject(FormBuilder);
+  private readonly userService = inject(UserService);
+  private readonly snackBar = inject(MatSnackBar);
+  protected isLoading = signal(false);
+  protected hide = signal(true);
 
+  form: FormGroup = this.fb.nonNullable.group({
+    emailId: ['', [Validators.required, Validators.email]],
+    password: ['', [Validators.required]],
+  });
+
+  onLogin() {
+    if (this.form.invalid) {
+      this.form.markAllAsTouched();
+      return;
+    }
+
+    this.isLoading.set(true);
+    const loginObj = {
+      emailId: this.form.value.emailId,
+      password: this.form.value.password,
+    };
+
+    console.log('Enviando...', loginObj);
+
+    this.userService.login(loginObj).subscribe({
+      next: (user) => {
+        this.isLoading.set(false);
+        this.mensagemUser(true);
+      },
+      error: (err) => {
+        this.isLoading.set(false);
+        this.mensagemUser(false, err);
+      },
+    });
+  }
+
+  mensagemUser(success: boolean = false, err: string = '') {
+    const message = success
+      ? 'Login redalizdo com sucesso!!!'
+      : 'Erro ao fazer login';
+
+    this.snackBar.open(message, 'Fechar', {
+      duration: 3000,
+      horizontalPosition: 'right',
+      verticalPosition: 'top',
+    });
+  }
+
+  clickEvent(event: MouseEvent) {
+    this.hide.set(!this.hide());
+    event.stopPropagation();
+  }
 }
