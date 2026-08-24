8cc0c8728f0d91e14b3b856a74f36ab70107a35a
Rafael Honorato <contatorafaelhonorato@gmail.com>
2026-08-24 17:59:36 -0300

    feat: atualização de rotas - Redirecionando usuários para a rota de login caso não esteja salvo em localStorage

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,8 +2,24 @@ import { Routes } from '@angular/router';
 
 export const routes: Routes = [
   {
+    path: '',
+    pathMatch: 'full',
+    redirectTo: 'auth',
+  },
+  {
     path: 'auth',
     loadChildren: () =>
       import('./features/auth/auth.routes').then((m) => m.AUTH_ROUTES),
+  },
+  {
+    path: 'dashboard',
+    loadChildren: () =>
+      import('./features/dashboard/dashboard.routes').then(
+        (m) => m.DASHBOARD_ROUTES,
+      ),
+  },
+  {
+    path: '**',
+    redirectTo: 'auth',
   },
 ];
