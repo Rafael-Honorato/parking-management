0991b29b1489e3143ab8d83d45a183cd6a80da3b
Rafael Honorato <contatorafaelhonorato@gmail.com>
2026-08-24 17:58:47 -0300

    feat: atualiação do authService - salvando usuário no localStorage ao fazer login

--- src/app/features/auth/services/auth.service.ts
+++ src/app/features/auth/services/auth.service.ts
@@ -1,21 +1,35 @@
 import { HttpClient } from '@angular/common/http';
-import { inject, Injectable } from '@angular/core';
+import { computed, inject, Injectable } from '@angular/core';
 import {
   LoginUserDTO,
   RegisterUserDTO,
   User,
 } from '../../../core/model/user.model';
 import { environment } from '../../../../environments/environment.development';
-import { Observable } from 'rxjs';
+import { Observable, tap } from 'rxjs';
+import { LocalStorageService } from './local-storage.service';
 
 @Injectable({
   providedIn: 'root',
 })
 export class AuthService {
   private readonly http = inject(HttpClient);
+  private readonly lsService = inject(LocalStorageService);
+
+  readonly user = this.lsService.user;
+
+  readonly isAuthenticated = computed(() => !!this.lsService.user());
 
   login(objLogin: LoginUserDTO): Observable<User> {
-    return this.http.post<User>(`${environment.BASE_URL}login`, objLogin);
+    return this.http.post<User>(`${environment.BASE_URL}login`, objLogin).pipe(
+      tap((user) => {
+        this.lsService.setUser(user);
+      }),
+    );
+  }
+
+  loggout() {
+    this.lsService.setUser(null);
   }
 
   register(objRegister: RegisterUserDTO): Observable<User> {
