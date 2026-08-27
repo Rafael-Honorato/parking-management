bed6311595fe3e0b1a1a664078c03a0c2a156477
Rafael Honorato <contatorafaelhonorato@gmail.com>
2026-08-27 14:49:54 -0300

    feat: remoção de imports não usados

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { MatSlideToggleModule } from '@angular/material/slide-toggle';
-import { MatButtonModule } from '@angular/material/button';
-import { MatCardModule } from '@angular/material/card';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, MatSlideToggleModule, MatButtonModule, MatCardModule],
+  imports: [RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
-export class AppComponent {
-  title = 'parking-management';
-}
+export class AppComponent {}
