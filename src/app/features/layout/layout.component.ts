7ef6df3fa1f721af72e65c2b571045555e67e5de
Rafael Honorato <contatorafaelhonorato@gmail.com>
2026-08-27 14:51:54 -0300

    feat: laout base para o nav e componentes

--- src/app/features/layout/layout.component.ts
+++ src/app/features/layout/layout.component.ts
@@ -1,11 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, signal, ViewChild } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { NavbarComponent } from '@app/shared/components/navbar/navbar.component';
+import { MatButtonModule } from '@angular/material/button';
+import { MatDrawer, MatSidenavModule } from '@angular/material/sidenav';
 
 @Component({
   selector: 'app-layout',
-  imports: [],
+  imports: [RouterOutlet, NavbarComponent, MatSidenavModule, MatButtonModule],
   templateUrl: './layout.component.html',
-  styleUrl: './layout.component.css'
+  styleUrl: './layout.component.css',
 })
 export class LayoutComponent {
+  menuAberto = signal(false);
 
+  onToggleMenu(estaAberto: boolean) {
+    this.menuAberto.set(estaAberto);
+    //console.log('O menu está: ', this.menuAberto() ? 'aberto' : 'fechado');
+  }
 }
