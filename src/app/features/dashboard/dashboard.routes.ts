7b172ac228d4c84fdb448373611a776df22c7fb6
Rafael Honorato <contatorafaelhonorato@gmail.com>
2026-08-27 14:51:11 -0300

    feat: correção de rota

--- src/app/features/dashboard/dashboard.routes.ts
+++ src/app/features/dashboard/dashboard.routes.ts
@@ -3,6 +3,7 @@ import { Routes } from '@angular/router';
 export const DASHBOARD_ROUTES: Routes = [
   {
     path: '',
+    pathMatch: 'full',
     loadComponent: () =>
       import('@app/features/dashboard/pages/dashboard/dashboard.component').then(
         (m) => m.DashboardComponent,
