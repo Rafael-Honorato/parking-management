94a567c9e1576780ce6e408ba27d439671b4d5c2
Rafael Honorato <contatorafaelhonorato@gmail.com>
2026-08-29 17:49:06 -0300

    feat: parkingService básico

--- src/app/core/services/parking.service.ts
+++ src/app/core/services/parking.service.ts
@@ -1,9 +1,14 @@
-import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { inject, Injectable } from '@angular/core';
+import { environment } from '../../../environments/environment.development';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ParkingService {
+  http = inject(HttpClient);
 
-  constructor() { }
+  getSitesByClientId(id: number) {
+    return this.http.get(`${environment.BASE_URL}GetSitesByClientId?id=${id}`);
+  }
 }
